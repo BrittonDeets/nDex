492b042b4966c07ae4085d04a4cac61507a9ba75
BrittonDeets <britton.deets@ncino.com>
2019-10-21 14:28:31 -0400

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+var tailwindcss = require('tailwindcss');
+
+module.exports = {
+  module: {
+    rules: [{
+      test: /\.scss$|\.sass$/,
+      use: [{
+        loader: 'postcss-loader',
+        options: {
+          plugins: [
+            tailwindcss('./tailwind.js')
+          ]
+        }
+      }]
+    }]
+  }
+}
